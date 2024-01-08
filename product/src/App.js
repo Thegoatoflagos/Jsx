9edff0ca28080fa4eababe66d890d55bd1b2bbf6
thegoatoflagos <thegoatoflagos@gmail.com>
2024-01-08 07:25:05 -0800

    first commit

---
+++ product/src/App.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import Name from "./components/Name";
+import Price from "./components/Price";
+import Description from "./components/Description";
+import Image from "./components/image";
+
+
+const App = () => {
+  return (
+    <div className="product">
+      <Name />
+      <Price />
+      <Description />
+      <Image />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ product/src/product.js
@@ -0,0 +1,8 @@
+export const product = {
+name: "Wireless Headphones",
+price: 230.45,
+description:
+ "Enjoy Immersive sound with these comfortable wireless headphones.",
+ imagePath: require("./Assets/headphones.avif")
+
+}
